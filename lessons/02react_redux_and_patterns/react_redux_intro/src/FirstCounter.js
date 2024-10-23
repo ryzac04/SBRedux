ea09b06be77312ee275b24dc6a7aa2ef7308af46
ryzac <rzc044@gmail.com>
2024-10-23 09:50:39 -0500

    Finished React Redux Part 1 videos.

---
+++ lessons/02react_redux_and_patterns/react_redux_intro/src/FirstCounter.js
@@ -0,0 +1,15 @@
+
+import { useSelector } from "react-redux";
+
+const FirstCounter = () => {
+    const count = useSelector(store => store.count); 
+
+    return (
+        <div>
+            <h1>Count is: {count}.</h1>
+        </div>
+    )
+
+}
+
+export default FirstCounter; 
---
+++ lessons/02react_redux_and_patterns/react_redux_intro/src/SecondCounter.js
@@ -0,0 +1,19 @@
+
+import { useSelector, useDispatch } from "react-redux";
+
+const SecondCounter = () => {
+    const dispatch = useDispatch(); 
+    const count = useSelector(store => store.count); 
+    const increment = () => dispatch({ type: "INCREMENT" });
+    const decrement = () => dispatch({ type: "DECREMENT" });
+    return (
+        <div>
+            <h1>Count is: {count}.</h1>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+        </div>
+    )
+
+}
+
+export default SecondCounter; 
